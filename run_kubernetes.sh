7a2531ce2bb035a506857fbb7459f195bb10657f
Arnold <kamstan6@gmail.com>
2021-09-21 15:49:22 -0400

    Task6: Deploy with kubernetes and save output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=karnold20/ml-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login
+kubectl run ml-prediction\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-prediction
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-prediction 8000:80
