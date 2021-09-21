d0681eccc89e2865dd7d758c157d176c54b94804
Arnold <kamstan6@gmail.com>
2021-09-21 14:56:37 -0400

    Task4: Uploaded the docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=karnold20/ml-prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker tag ml-prediction:latest karnold20/ml-prediction:latest
 # Step 3:
 # Push image to a docker repository
+docker push karnold20/ml-prediction:latest
